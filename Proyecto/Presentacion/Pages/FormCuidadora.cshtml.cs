70ef52cf3aebf35e7b98cc1798db62cbf8eda2b1
dipamato <dipamato@gmail.com>
2023-09-27 14:19:07 -0500

    Presentacion Capa

---
+++ Proyecto/Presentacion/Pages/FormCuidadora.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentacion.Pages
+{
+    public class FormCuidadoraModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
